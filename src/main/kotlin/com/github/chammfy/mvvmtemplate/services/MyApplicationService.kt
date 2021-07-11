ba8e331c08b1eea0086e2b38fe00c04510f4cc99
GitHub Action <action@github.com>
2021-07-11 15:09:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/chammfy/mvvmtemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/chammfy/mvvmtemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.chammfy.mvvmtemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.chammfy.mvvmtemplate.MyBundle
 
 class MyApplicationService {
 
